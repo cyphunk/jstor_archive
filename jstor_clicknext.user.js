980cd6af0c6f006e318fd916a9cf2db224332c17
Nathan Fain <cyphunk@gmail.com>
2014-08-06 12:09:57 +0200

    r.i.p.

--- jstor_clicknext.user.js
+++ jstor_clicknext.user.js
@@ -1,1 +1,11 @@
-setTimeout("$('.next')[1].click()", 6200);
+// ==UserScript==
+// @name        jstor_clicknext
+// @namespace   www.jstor.org
+// @downloadURL	https://github.com/cyphunk/jstor_archive/raw/master/jstor_clicknext.user.js
+// @description view jstor article and auto click through to next pages
+// @version     1
+// @grant       none
+// @include	http://www.jstor.org/discover/*
+// ==/UserScript==
+
+setTimeout("$('.next')[1].click()" , 5000);
