cf08155a32982bd0412ccc85df87c8cc420b5856
Nathan Fain <cyphunk@gmail.com>
2014-08-07 03:41:27 +0200

    r.i.p.

--- README.md
+++ README.md
@@ -14,16 +14,19 @@ articles contents.
 
 # Use
 
-Defaults are configured for Firefox browsers on Linux systems. For OSX change the ``CACHE_DIR`` path appropriately. 
+Defaults are configured for Firefox browsers on Linux systems or Chrome on OSX.
+For other browsers or systems set the ``CACHE_DIR`` path appropriately.
 
 1. ``git clone https://github.com/cyphunk/jstor_archive.git``
-2. Install Javascript userscript via User Script manager (such as grease 
-   monkey) or directly if your browser supports it by accessing the raw 
+2. Install Javascript userscript via User Script manager (such as grease
+   monkey) or directly if your browser supports it by accessing the raw
    [jstor_clicknext.user.js](https://github.com/cyphunk/jstor_archive/raw/master/jstor_clicknext.user.js).
-3. ``cd jstor_archive`` and ``./cache_monitor.sh EXAMPLE_NAME`` where ``EXAMPLE_NAME`` is the directory to be created within the current directory for storing GIF's. This name will also be used as the name for the PDF and HTML indexes that will be created later.
-4. If the monitored ``CACHE_DIR`` shown on execution is different from that of your 
-   browser (tested with Linux Firefox) change the ``CACHE_DIR`` enviornment
-   variable and execute again.
+3. ``cd jstor_archive`` and ``./cache_monitor.sh EXAMPLE_NAME`` where
+   ``EXAMPLE_NAME`` is the directory to be created within the current directory
+   for storing GIF's. This name will also be used as the name for the PDF and
+   HTML indexes that will be created later.
+4. If the monitored ``CACHE_DIR`` shown on execution is different from that of
+   your browser change the ``CACHE_DIR`` environment variable and execute again.
 5. Open a JSTOR article in your browser.
 6. When all pages of the article have been viewed, either by manually
    clicking through each page or by letting the ``jstor_clicknext.user.js``
--- cache_monitor.sh
+++ cache_monitor.sh
@@ -4,12 +4,21 @@ usage() {
 	cat << EOM
 
 Script monitors CACHE_DIR for gif's with width GIF_WIDTH
-Copies them to a directed <name> (passed as argument to 
-script). When script is interrupted (ctrl+c) it then 
+Copies them to a directed <name> (passed as argument to
+script). When script is interrupted (ctrl+c) it then
 creates a <name>.pdf and <name>.html
 
-usage: $0 <name>
-usage: CACHE_DIR=~/.cache/mozilla GIF_WIDTH=760 $0 <name>
+Examples:
+
+  usage: $0 <name>
+
+Linux Firefox example:
+
+  usage: CACHE_DIR=~/.cache/mozilla GIF_WIDTH=760 $0 <name>
+
+OSX Chrome example:
+
+  usage: CACHE_DIR=~/Library/Caches/Google/Chrome/ GIF_WIDTH=760 $0 <name>
 
 current enviornment settings:
 CACHE_DIR = ${CACHE_DIR}
@@ -20,7 +29,13 @@ exit 1
 }
 
 if [ -z "${CACHE_DIR}" ]; then
-	CACHE_DIR="$HOME/.cache/mozilla/firefox/*.default"
+	if [ `uname` == "Linux" ]; then
+		CACHE_DIR="$HOME/.cache/mozilla/firefox/*.default"
+	elif [ `uname` == 'Darwin' ]; then
+		CACHE_DIR="$HOME/Library/Caches/Google/Chrome/"
+	else
+		CACHE_DIR="set CACHE_DIR manually"
+	fi
 fi
 if [ -z "${GIF_WIDTH}" ]; then
 	GIF_WIDTH=760
@@ -32,11 +47,11 @@ fi
 
 STORE_NAME="$1"
 STORE_DIR=`pwd`/$STORE_NAME
+mkdir $STORE_DIR
 echo "Monitoring CACHE_DIR: ${CACHE_DIR}"
 echo "Will store all GIF's with width GIF_WIDTH: ${GIF_WIDTH} in"
 echo "$STORE_DIR"
 cd $CACHE_DIR
-
 
 halt () {
 	echo "*** stop ***"
@@ -52,7 +67,7 @@ halt () {
 	echo "*** creating ${STORE_NAME}.html ***"
 	echo "<html><head><title>JsToR archive - ${STORE_NAME}</title></head><body>" > $STORE_DIR/${STORE_NAME}.html
 	for gif in `ls $STORE_DIR/*.gif`; do
-		echo "<img src=\"${gif}\"><br>" >> $STORE_DIR/${STORE_NAME}.html	
+		echo "<img src=\"${gif}\"><br>" >> $STORE_DIR/${STORE_NAME}.html
 	done
 	echo "</body></html" >> $STORE_DIR/${STORE_NAME}.html
 
@@ -65,7 +80,7 @@ while [ 1 ]; do
 	files=$(find . -type f -newer $STORE_DIR -exec file {} \; | grep GIF | grep 760 | awk '{print $1}')
 	for file in $files; do
 		# get rid of : from mime string
-		filepath=${file%?} 
+		filepath=${file%?}
 		filename=$(basename $filepath)
 		if [ ! -e "$STORE_DIR/*_$filename*" ]; then
 			echo "cp $filepath $STORE_DIR/$(date +%H%M%S)_${filename}.gif"
@@ -77,4 +92,3 @@ while [ 1 ]; do
 done
 
 sleep 1;
-
